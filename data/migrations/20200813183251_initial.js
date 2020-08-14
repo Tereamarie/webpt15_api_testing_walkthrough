90772b9e19a84a66fee2df5423b48ecdcee73aec
Anthony Sutton <anthony.sutton95@gmail.com>
2020-08-13 18:38:05 -0700

    feat(migrations): Setup migration table

---
+++ data/migrations/20200813183251_initial.js
@@ -0,0 +1,12 @@
+exports.up = async function (knex) {
+  await knex.schema.createTable("animals", (table) => {
+    table.increments("id");
+    table.text("name").notNullable();
+    table.text("species").notNullable();
+    table.float("age").notNullable();
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.dropTableIfExists("animals");
+};
